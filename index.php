ce5fd619a7934114eabcef6e7d5d304725b9614a
DorinVieru <dorino.vieru@gmail.com>
2024-01-26 14:04:54 +0100

    [UP] Create necessary files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- SEZIONE PHP LOGICA -->
+<?php
+
+
+?>
+
+<!-- SEZIONE HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
